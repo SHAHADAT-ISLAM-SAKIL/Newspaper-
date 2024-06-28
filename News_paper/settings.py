255a43106701360d8170e0769bf39663c970ab6c
MD SHAHADAT ISLAM SAKIL <129543567+SHAHADAT-ISLAM-SAKIL@users.noreply.github.com>
2024-06-28 11:45:18 +0600

    Update settings.py

--- News_paper/settings.py
+++ News_paper/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt',
     'rest_framework',
     'django_filters',
     'news',
